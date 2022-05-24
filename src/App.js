6b0bbcd9f0430bf0ce9f3b30d4355728c92be331
NewChakrit <imnew108@gmail.com>
2022-05-24 01:23:02 -0700

    'fix-sidebar'

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ const App = () => {
             </TooltipComponent>
           </div>
           {activeMenu ? (
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+            <div className="w-72 fixed sidebar z-50 dark:bg-secondary-dark-bg bg-white ">
               <Sidebar />
             </div>
           ) : (
